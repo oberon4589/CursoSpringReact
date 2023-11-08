b73880245f78957263410715fde1c941961f2fe6
SKINKA <oberon4589@gmail.com>
2023-11-08 20:50:07 -0300

    feat: criando rota para listar produtos

--- src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -1,10 +1,22 @@
 package br.com.api.produtos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.services.ProdutoService;
+
 @RestController
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModel> listar() { // Pega a lista de produtos em ProdutoService
+        return ps.listar();
+    }
 
     @GetMapping("/")
     public String rota() {
