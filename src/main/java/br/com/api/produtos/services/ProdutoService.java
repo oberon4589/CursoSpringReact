20de367795f7e56ede8be083582e90a92e99f72d
SKINKA <oberon4589@gmail.com>
2023-11-08 20:44:10 -0300

    feat: criando método para listar todos os produtos

--- src/main/java/br/com/api/produtos/services/ProdutoService.java
+++ src/main/java/br/com/api/produtos/services/ProdutoService.java
@@ -1,5 +1,20 @@
 package br.com.api.produtos.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.repository.ProdutoRepository;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository pr;
+
+    // Método para listar todos os produtos
+    public Iterable<ProdutoModel> listar() {
+        return pr.findAll();
+    }
 
 }
