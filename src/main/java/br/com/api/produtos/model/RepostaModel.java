4bd149ea0bebe7840c2fad0761cd9693e0c7b280
SKINKA <oberon4589@gmail.com>
2023-11-08 20:43:47 -0300

    feat: lidando com mensagens de erro

--- src/main/java/br/com/api/produtos/model/RepostaModel.java
+++ src/main/java/br/com/api/produtos/model/RepostaModel.java
@@ -1,5 +1,13 @@
 package br.com.api.produtos.model;
 
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component // Pode adicionar injeção de dependências
+@Data
 public class RepostaModel {
+
+    private String mensagem;
 
 }
