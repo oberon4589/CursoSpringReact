f2acd3fe766a44596170d5318eb1a74bacebb13a
SKINKA <oberon4589@gmail.com>
2023-11-08 20:35:26 -0300

    feat: adicionando rotas

--- src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -1,5 +1,14 @@
 package br.com.api.produtos.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProdutoController {
+
+    @GetMapping("/")
+    public String rota() {
+        return "api funcionando!";
+    }
 
 }
