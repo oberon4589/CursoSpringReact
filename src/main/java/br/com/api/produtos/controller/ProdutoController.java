d3bc8d85092954e4372a0de1f163de0e697d96f9
SKINKA <oberon4589@gmail.com>
2023-11-09 13:22:52 -0300

    feat: rota para alterar produtos

--- src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService ps;
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModel pm) {
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModel pm) {
